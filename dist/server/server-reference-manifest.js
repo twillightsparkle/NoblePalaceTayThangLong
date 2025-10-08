7c0daeaab8d642e5bce6744dbe6ae834fe482d75
unknown <Thang.Tran@student.oulu.fi>
2025-10-08 16:31:38 +0300

    Fix GitHub Pages deployment: Add basePath and assetPrefix for proper asset loading

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eocCWH8buzMFqI09a3E9f3TsNR8LqZVXOq9Tq+v9vkc=\"\n}"
--- dist/static/chunks/src_app_vi-tri_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_b7b356d5._.js",
-  "static/chunks/src_components_Navbar_tsx_e572545a._.js"
-],
-    source: "dynamic"
-});
