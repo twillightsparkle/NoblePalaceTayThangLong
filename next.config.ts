c0f9c352fae83fc661270cdf0de7c72160dbfc1a
unknown <Thang.Tran@student.oulu.fi>
2025-10-08 18:19:01 +0300

    turn production true in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = true;
 const repoName = 'NoblePalaceTayThangLong';
 
 const nextConfig: NextConfig = {
