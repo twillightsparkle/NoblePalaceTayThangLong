bbc3522d7adbff89d14eda8b159ee2e8cd6f35a5
unknown <Thang.Tran@student.oulu.fi>
2025-10-15 16:14:39 +0300

    change gitignore file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = true;
+const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'NoblePalaceTayThangLong';
 
 const nextConfig: NextConfig = {
